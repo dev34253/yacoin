248c28cf7666127733cba59ef79f133b2f9b834d
Chau Thai Nguyen <chaunguyen4392@gmail.com>
2020-11-25 22:00:10 +0700

    Adjust granularity

--- src/main.h
+++ src/main.h
@@ -383,22 +383,22 @@ public:
     static const uint32_t SEQUENCE_LOCKTIME_DISABLE_FLAG = (1 << 31);
 
     /* If CTxIn::nSequence encodes a relative lock-time and this flag
-     * is set, the relative lock-time has units of 512 seconds,
+     * is set, the relative lock-time has units of 1 seconds,
      * otherwise it specifies blocks with a granularity of 1. */
-    static const uint32_t SEQUENCE_LOCKTIME_TYPE_FLAG = (1 << 22);
+    static const uint32_t SEQUENCE_LOCKTIME_TYPE_FLAG = (1 << 30);
 
     /* If CTxIn::nSequence encodes a relative lock-time, this mask is
      * applied to extract that lock-time from the sequence field. */
-    static const uint32_t SEQUENCE_LOCKTIME_MASK = 0x0000ffff;
+    static const uint32_t SEQUENCE_LOCKTIME_MASK = 0x2fffffff;
 
     /* In order to use the same number of bits to encode roughly the
      * same wall-clock duration, and because blocks are naturally
-     * limited to occur every 600s on average, the minimum granularity
-     * for time-based relative lock-time is fixed at 512 seconds.
+     * limited to occur every 60s on average, the minimum granularity
+     * for time-based relative lock-time is fixed at 1 seconds.
      * Converting from CTxIn::nSequence to seconds is performed by
-     * multiplying by 512 = 2^9, or equivalently shifting up by
-     * 9 bits. */
-    static const int SEQUENCE_LOCKTIME_GRANULARITY = 9;
+     * multiplying by 1 = 2^0, or equivalently shifting up by
+     * 0 bits. */
+    static const int SEQUENCE_LOCKTIME_GRANULARITY = 0;
 
     CTxIn()
     {
